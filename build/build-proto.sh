bfc64c45ca52b42f08b6dd729a8c98c58bfb26fa
Steve Winslow <swinslow@gmail.com>
2019-01-27 16:27:23 -0500

    Fix protobuf import paths
    Signed-off-by: Steve Winslow <swinslow@gmail.com>

--- build/build-proto.sh
+++ build/build-proto.sh
@@ -5,5 +5,5 @@
 # Generates Golang protobuf code from .proto files.
 # Should be run from the top-level peridot-core directory.
 
-protoc -I ./ pkg/agent/agent.proto --go_out=plugins=grpc:.
-protoc -I ./ pkg/controller/controller.proto --go_out=plugins=grpc:.
+protoc -I ./ ./pkg/agent/agent.proto --go_out=plugins=grpc,paths=source_relative:.
+protoc -I ./ ./pkg/controller/controller.proto --go_out=plugins=grpc,paths=source_relative:.
